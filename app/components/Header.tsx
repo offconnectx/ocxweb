cbc08cbbc6360f3f4806aa624e7fcc1b5cb6b345
luqxus <lebelwane@protonmail.com>
2024-09-25 01:57:19 +0200

    optimize header for mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,30 +5,35 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="w-full bg-gradient-to-r from-white to-pink-200 rounded-3xl">
-      <div className="flex items-center justify-between mx-auto max-w-6xl py-16 gap-x-10">
-        <div className="flex flex-col gap-y-10">
-          <p className="text-5xl font-mono font-bold">
+      <div className="flex flex-col md:flex-row items-center justify-between mx-auto max-w-6xl py-8 md:py-16 gap-y-10 md:gap-x-10">
+        <div className="flex flex-col gap-y-6 md:gap-y-10 text-center md:text-left">
+          <p className="text-3xl md:text-5xl font-mono font-bold">
             {`Offline transactions with speed and reliability.`}
           </p>
-          <p className="text-lg font-mono text-gray-600">
-            {`Our app ensures that your payments remain seamless, even without
-            internet access. Whether you're in remote areas or experiencing
-            network disruptions, you can rely on fast, secure, and efficient
-            transactions.`}
+          <p className="text-base md:text-lg font-mono text-gray-600">
+            {`Our app ensures that your payments remain seamless, even without internet access. Whether you're in remote areas or experiencing network disruptions, you can rely on fast, secure, and efficient transactions.`}
           </p>
           <GooglePlayDownloadButton />
-
           <br />
-          <div className="flex items-center">
-            <p className="text-gray-600 text-xl font-mono align-bottom">
+          <div className="flex justify-center md:justify-start items-center gap-x-2">
+            <p className="text-gray-600 text-lg md:text-xl font-mono">
               {`Built on `}
             </p>
             <Link href={"https://lisk.com/"}>
-              <Image height={100} width={100} src={"/lisk.png"} alt="" />
+              <Image height={50} width={50} src={"/lisk.png"} alt="Lisk Logo" />
             </Link>
           </div>
         </div>
-        <Image height={450} width={450} src="/mock_1.png" alt=" " />
+        <div className="flex justify-center md:justify-end">
+          <Image
+            height={300}
+            width={300}
+            // md:height={450}
+            // md:width={450}
+            src="/mock_1.png"
+            alt="App Mockup"
+          />
+        </div>
       </div>
     </div>
   );
